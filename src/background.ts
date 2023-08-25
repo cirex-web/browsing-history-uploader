7220b9182deae8771d37f64ad08f8fcefe46e01e
topshenyi-web <erxu@ctemc.org>
2023-08-25 13:31:02 -0400

    very rough commit

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,24 @@
-function polling() {
-  // console.log("polling");
-  setTimeout(polling, 1000 * 30);
-}
 
-polling();
+const addTab = async (tabId: number) => {
+  const tabData = await chrome.tabs.get(tabId);
+  if (tabData.status === "complete" && tabData.url && tabData.active) {
+
+    console.log(tabData.url, tabData.title);
+    fetch("https://cirex-dev-default-rtdb.firebaseio.com/history.json", {
+      method: "POST",
+      body: JSON.stringify({
+        tabId: tabId,
+        url: new URL(tabData.url).origin,
+        title: tabData.title,
+        favicon: tabData.favIconUrl,
+        time: +new Date()
+
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+  }
+}
+chrome.tabs.onActivated.addListener((tab) => addTab(tab.tabId));
+chrome.tabs.onUpdated.addListener(addTab);
